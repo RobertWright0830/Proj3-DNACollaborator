4a0ff2a3b9cdffdedb43dffeb96d3e44351288ab
RobertWright0830 <dnafrankenstein@gmail.com>
2024-02-09 08:41:26 -0600

    commented out get and post on server.js

--- server/server.js
+++ server/server.js
@@ -32,55 +32,55 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
   
-  app.get('/', async (req, res) => {
-    try {
-      const data = await chromosomeSegment.find({});
-      res.json(data);
-    } catch (err) {
-      res.status(500).send({ message: 'Error fetching data', error: err });
-    }
-  });
+//   app.get('/', async (req, res) => {
+//     try {
+//       const data = await chromosomeSegment.find({});
+//       res.json(data);
+//     } catch (err) {
+//       res.status(500).send({ message: 'Error fetching data', error: err });
+//     }
+//   });
     
-  app.post('/upload', upload.single('file'), async (req, res) => {
-try {
-  console.log("Received file:", req.file.path);
-  const jsonObj = await csv().fromFile(req.file.path);
-  console.log("Parsed CSV:", jsonObj);
+//   app.post('/upload', upload.single('file'), async (req, res) => {
+// try {
+//   console.log("Received file:", req.file.path);
+//   const jsonObj = await csv().fromFile(req.file.path);
+//   console.log("Parsed CSV:", jsonObj);
 
-  const chromosomeInfo = jsonObj.map(item => ({
-    testerId: item['PrimaryKit'],
-    matchId: item['MatchedKit'],
-    chromosome: item['chr'],
-    start: parseInt(item['B37Start'], 10),
-    end: parseInt(item['B37End'], 10),
-    segmentCM: parseFloat(item['Segment cM']),
-    snp: parseInt(item['SNPs'], 10),
-    matchName: item['MatchedName'],
-    sex: item['Matched Sex'],
-    matchEmail: item['MatchedEmail'],
-  }));
-  console.log("Mapped data for MongoDB:", chromosomeInfo);
+//   const chromosomeInfo = jsonObj.map(item => ({
+//     testerId: item['PrimaryKit'],
+//     matchId: item['MatchedKit'],
+//     chromosome: item['chr'],
+//     start: parseInt(item['B37Start'], 10),
+//     end: parseInt(item['B37End'], 10),
+//     segmentCM: parseFloat(item['Segment cM']),
+//     snp: parseInt(item['SNPs'], 10),
+//     matchName: item['MatchedName'],
+//     sex: item['Matched Sex'],
+//     matchEmail: item['MatchedEmail'],
+//   }));
+//   console.log("Mapped data for MongoDB:", chromosomeInfo);
 
-  await chromosomeSegment.insertMany(chromosomeInfo);
-  console.log("Data inserted into MongoDB");
+//   await chromosomeSegment.insertMany(chromosomeInfo);
+//   console.log("Data inserted into MongoDB");
 
-  res.status(200).send({
-      message: 'Data uploaded successfully',
-      });
+//   res.status(200).send({
+//       message: 'Data uploaded successfully',
+//       });
 
-      fs.unlink(req.file.path, err => {
-        if (err) console.error("Error deleting file:", err);
-        else console.log("File deleted successfully");
-        });
+//       fs.unlink(req.file.path, err => {
+//         if (err) console.error("Error deleting file:", err);
+//         else console.log("File deleted successfully");
+//         });
 
-      } catch (err) {
-        console.error("Error uploading data:", err);
-    res.status(500).send({
-          message: 'Data upload failed',
-          error: err
-        });
-      }
-    });
+//       } catch (err) {
+//         console.error("Error uploading data:", err);
+//     res.status(500).send({
+//           message: 'Data upload failed',
+//           error: err
+//         });
+//       }
+//     });
 
   app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
